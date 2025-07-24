5ef6f5eb0e1300ecf694ce1040d99c35343195b8
sophrone <46854009+sophrone@users.noreply.github.com>
2025-07-24 19:38:26 +0100

    Fix hydration error: Use state to control 'Campus' dropdown, remove arrow, match font size and weight, verify mobile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,6 +7,7 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <header className={styles.header}>
@@ -43,39 +44,47 @@ export default function Navbar() {
             </Link>
           </li>
           <li className={styles.dropdown}>
-            <button className={styles.dropdownButton}>Campus</button>
-            <ul className={styles.dropdownMenu}>
-              <li>
-                <Link href="/campus/apapa" className={styles.dropdownLink}>
-                  Apapa Campus
-                </Link>
-              </li>
-              <li>
-                <Link href="/campus/ilasa" className={styles.dropdownLink}>
-                  Ilasa Campus
-                </Link>
-              </li>
-              <li>
-                <Link href="/campus/kano" className={styles.dropdownLink}>
-                  Kano Campus
-                </Link>
-              </li>
-              <li>
-                <Link href="/campus/warri" className={styles.dropdownLink}>
-                  Warri Campus
-                </Link>
-              </li>
-              <li>
-                <Link href="/campus/oyo" className={styles.dropdownLink}>
-                  Oyo Campus
-                </Link>
-              </li>
-              <li>
-                <Link href="/campus/secondary-school" className={styles.dropdownLink}>
-                  Secondary School Campuses
-                </Link>
-              </li>
-            </ul>
+            <button
+              className={styles.dropdownButton}
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              aria-expanded={isDropdownOpen}
+            >
+              Campus
+            </button>
+            {isDropdownOpen && (
+              <ul className={styles.dropdownMenu}>
+                <li>
+                  <Link href="/campus/apapa" className={styles.dropdownLink}>
+                    Apapa Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/campus/ilasa" className={styles.dropdownLink}>
+                    Ilasa Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/campus/kano" className={styles.dropdownLink}>
+                    Kano Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/campus/warri" className={styles.dropdownLink}>
+                    Warri Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/campus/oyo" className={styles.dropdownLink}>
+                    Oyo Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/campus/secondary-school" className={styles.dropdownLink}>
+                    Secondary School Campuses
+                  </Link>
+                </li>
+              </ul>
+            )}
           </li>
           <li>
             <Link href="/contact-us" className={styles.link}>
