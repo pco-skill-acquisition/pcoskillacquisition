7ffc79543cd9e781f04212db4c29ae74a3e060c6
sophrone <46854009+sophrone@users.noreply.github.com>
2025-07-24 17:35:17 +0100

    Confirm layout.tsx with no font link, relying on _document.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
-      </head>
       <body>
         <Navbar />
         <main className="container">{children}</main>
